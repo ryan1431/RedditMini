5f518efcd1be139b568121834395c200804109b1
Ryan1431 <ryan1431@gmail.com>
2022-11-12 16:44:42 -0500

    Store feedposts in redux store, use async thunk to handle fetch and format

--- src/app/hooks/useFeed.ts
+++ src/app/hooks/useFeed.ts
@@ -1,104 +1,52 @@
-import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
-import { setQuery } from "../reducers/querySlice";
-import { getFeedPosts, PostType } from "../../utility";
+import { useCallback, useEffect, useMemo, useRef } from "react";
+import { fetchFeed, setFeedPosts, setQuery } from "../reducers/querySlice";
 import { useAppDispatch, useAppSelector } from "./hooks";
 import { base } from "../../utility/data";
 
 export type SortField = 'best' | 'hot' | 'new' | 'top' | 'rising';
 export type FeedField = 'home' | 'custom' | 'saved';
 
-export const useFeed = (setFeedPosts: React.Dispatch<React.SetStateAction<PostType[]>>, isVisible: boolean) => {
+export const useFeed = (isVisible: boolean) => {
   const dispatch = useAppDispatch();
 
-  const searchQueries = useAppSelector((s) => s.query);
   const savedPosts = useAppSelector((state) => state.saved.savedPosts);
-  const {after, feed, sort} = useAppSelector((state) => state.query);
+  const { feed, sort } = useAppSelector((state) => state.query);
   const subs = useAppSelector((state) => state.subreddits.subs);
 
-  const [loading, setLoading] = useState<boolean>(true);
-  const [sortField, setSortField] = useState<SortField>(searchQueries.sort as SortField);
-  const [feedField, setFeedField] = useState<FeedField>(searchQueries.feed as FeedField);
-  const [adding, setAdding] = useState<boolean>(false);
-
-  // Prevent useEffect loop
-  const srNames = useMemo<string[]>(() => subs.map((s) => s.name), [subs]);
-
   const currentUrl = useRef<string>('');
-  const timeoutQueue = useRef<NodeJS.Timeout[]>([]);
+  const srNames = useMemo<string[]>(() => subs.map((s) => s.name), [subs]);
   
-  
-  // Change sort/feed state
   const sortBy = useCallback(({target}:any) => {
-    let type = target.classList[0];
+    if (target.value === sort || target.value === feed) return;
 
-    // Button is already clicked
-    if ((type === 'sort' && target.value === sortField) || 
-       (type === 'feed' && target.value === feedField)) return;
-
-    setFeedPosts([]);
-    timeoutQueue.current.forEach((t) => clearTimeout(t));
-    timeoutQueue.current = [];
-
-    (type === 'sort')
-      ? setSortField(target.value)
-      : setFeedField(target.value);
-      
-    dispatch(setQuery([target.value, type]));
-    setLoading(true);
-  }, [sortField, feedField, setFeedPosts, timeoutQueue, dispatch]);
+    dispatch(setQuery(['after', '']));
+    dispatch(setFeedPosts([]));
+    dispatch(setQuery([target.classList[0], target.value]));
+  }, [sort, feed, dispatch]);
 
   useEffect(() => {
-    if (adding || !loading) return;
     if (feed === 'saved') {
-      setFeedPosts(savedPosts);
-      setLoading(false);
+      dispatch(setFeedPosts(savedPosts));
       return;
     } else if (feed === 'custom' && !srNames.length) {
-      setLoading(false);
+      dispatch(setFeedPosts([]));
       return;
     }
 
     let url = base + ((feed === 'custom' && `r/${srNames.join('+')}/`) || '');
     currentUrl.current = url + sort;
-
-    // Fetch posts
-    getFeedPosts(`${currentUrl.current}?limit=10`)
-    .then((res) => {
-      setFeedPosts(res.posts);
-      if (res.after) {
-        dispatch(setQuery([res.after, 'after']));
-      }
-      timeoutQueue.current.push(setTimeout(() => {
-        setLoading(false);
-      }, 3000));
-    });
-  }, [adding, dispatch, feed, loading, savedPosts, setFeedPosts, sort, srNames]);
+    
+    dispatch(fetchFeed(currentUrl.current));
+  }, [dispatch, feed, savedPosts, sort, srNames]);
 
   // Infinite scroll
   useEffect(() => {
-    if (isVisible) {
-      setLoading(true);
-      setAdding(true);
-      getFeedPosts(`${currentUrl.current}?limit=10&after=${after}`)
-        .then((res) => {
-          setFeedPosts((prev) => [...prev, ...res.posts]);
-          if (res.after) {
-            dispatch(setQuery([res.after, 'after']));
-          }
-          timeoutQueue.current.push(setTimeout(() => {
-            setLoading(false);
-            setAdding(false);
-          }, 3000))
-      })
-    }
-  }, [isVisible, after, dispatch, setFeedPosts]);
+    if (isVisible) dispatch(fetchFeed(currentUrl.current));
+  }, [isVisible, dispatch]);
 
   return {
     sortBy,
     savedPosts,
-    loading,
     feed,
-    feedField,
-    sortField,
   }
 }
--- src/app/reducers/querySlice.ts
+++ src/app/reducers/querySlice.ts
@@ -1,36 +1,85 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit"
+import { getFeedPosts, PostType } from "../../utility";
+import { RootState } from "../store/store";
 interface QueryState {
   after: string,
   feed: string,
   sort: string,
+  feedPosts: PostType[],
+  fetching: boolean,
+  add: boolean,
 }
+
+type SetQuery = 'after' | 'feed' | 'sort';
 
 export const initialState: QueryState = {
   after: '',
   feed: 'home',
   sort: 'best',
+  feedPosts: [],
+  fetching: false,
+  add: false,
 }
 
 let savedState: QueryState | undefined;
-
 try {
   savedState = {...JSON.parse(localStorage.getItem('query') as string), after: ''} as QueryState;
 } catch(e) {
   // No saved state
 }
 
+export const fetchFeed = createAsyncThunk(
+  'query/fetchFeed',
+  async (url: string, _api) => {
+    const after = (_api.getState() as RootState).query.after;
+    const params = new URLSearchParams();
+
+    params.set('limit', '10');
+    if (after) { 
+      params.set('after', after);
+      _api.dispatch(setAdd(true));
+    }
+
+    const response = await getFeedPosts(`${url}?${params.toString()}`);
+    return response;
+  }
+)
+
 export const queryReducer = createSlice({
   name: 'query',
   initialState: {...initialState, ...savedState} || initialState,
   reducers: {
-    setQuery: (state, action: PayloadAction<[string, keyof QueryState]>) => {
-      state[action.payload[1]] = action.payload[0];
+    setQuery: (state, action: PayloadAction<[SetQuery, string]>) => {
+      state[action.payload[0]] = action.payload[1];
     },
+    setFeedPosts: (state, action: PayloadAction<PostType[]>) => {
+      state.feedPosts = action.payload;
+    },
+    setAdd: (state, action: PayloadAction<boolean>) => {
+      state.add = action.payload;
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchFeed.pending, (state, action) => {
+        state.fetching = true;
+      })
+      .addCase(fetchFeed.fulfilled, (state, action: PayloadAction<{posts: PostType[],after: string}>) => {
+        const res = action.payload;
+        
+        state.fetching = false;
+        state.after = res.after;
+
+        state.feedPosts = state.add
+          ? [...state.feedPosts, ...res.posts]
+          : res.posts;
+
+        state.add = false;
+      });
   }
 });
 
-
 export { savedState }
-export const { setQuery } = queryReducer.actions;
+export const { setQuery, setFeedPosts, setAdd } = queryReducer.actions;
 
 export default queryReducer.reducer;
