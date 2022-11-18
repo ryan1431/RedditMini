9b9f98886ba34808b86170a71421f6ceae3ebfab
Ryan1431 <ryan1431@gmail.com>
2022-11-18 10:08:09 -0500

    Make Comment component recurse itself for replies. useRef to getBoundingRect() and create close bars for each chain

--- src/components/feed/Comment.tsx
+++ src/components/feed/Comment.tsx
@@ -1,3 +1,5 @@
+import clsx from 'clsx';
+import { useEffect, useRef, useState } from 'react';
 import { CommentData } from '../../types/commentType';
 import './Comment.css';
 
@@ -7,13 +9,48 @@ interface CommentProps {
 
 export const Comment = ({comment}: CommentProps) => {
 
+  const commentRef = useRef<HTMLDivElement>(undefined!);
+  const [bounding, setBounding] = useState<DOMRect>();
 
+  useEffect(() => {
+    setBounding(commentRef.current.getBoundingClientRect());
+  }, []);
   
   return (
-    <div 
-      className={`comment depth-${comment.depth}`} 
-      dangerouslySetInnerHTML={{__html: comment.body_html}} 
-      style={{marginLeft: 10 + (comment.depth * 15)}}  
-    />
+    // Tree
+    <div className={clsx({'comment-chain': true})} >
+
+      {/* Comment */}
+      <div 
+        className={`comment depth-${comment.depth}`} 
+        dangerouslySetInnerHTML={{__html: comment.body_html}} 
+        style={{marginLeft: 10 + (comment.depth * 15)}}  
+        ref={commentRef}
+      />
+
+      {/* Close chain bar */}
+      {comment.replies && <div className='comment-chain-close'
+        style={{
+          height: `calc(100% - ${bounding && bounding.height}px - 10px)`,
+          left: `${11 + (comment.depth * 15)}px`,
+        }}
+      ></div> }
+
+      {/* Replies */}
+      {comment.replies && comment.replies.map((comment) => {
+        if (comment.kind === 't1') { 
+          return (
+            <Comment comment={comment.data as CommentData} />
+          )
+
+        } else {
+          return (
+            <>
+
+            </>
+          )
+        }
+      })}
+    </div>
   )
 }
--- src/components/feed/OpenPost.tsx
+++ src/components/feed/OpenPost.tsx
@@ -1,13 +1,11 @@
 import './OpenPost.css';
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { PostType } from '../../utility';
 import { Post } from './post/Post';
 import { useAppDispatch, useAppSelector } from '../../app/hooks/hooks';
 import { fetchComments } from '../../app/reducers/commentsSlice';
 import type { CommentData, CommentType } from '../../types/commentType';
 import { Comment } from './Comment';
-import clsx from 'clsx';
-
 interface OpenPostProps {
   post: PostType,
 }
@@ -30,44 +28,7 @@ export const OpenPost = ({post}:  OpenPostProps) => {
     dispatch(fetchComments({ url: post.link, postId: post.name}))
   }, [comments, commentsState, dispatch, lastPostId, post.link, post.name]);
 
-  const renderCommentsRecurse = (comments: CommentType[]) => {
-
-    
-    return (
-      <>
-        {comments.map((comment) => {
-          if (comment.kind === 't1') {
-            let data = comment.data as CommentData;
-            let id = `depth-${data.depth}_${data.id}`;
-            return (
-              <div className={clsx({'comment-chain': !data.depth})}>
-                
-                <div id={id}>
-                  <Comment key={data.id} comment={data} />
-                </div>
-                {!data.depth && data.replies.length && <div className='comment-chain-close'
-                  style={{
-                    height: `calc(100% - ${document.querySelector(`#${id}`)?.getBoundingClientRect().height}px - 10px)`
-                  }}
-                ></div> }
-
-                {data.replies && renderCommentsRecurse(data.replies)}
-
-              </div>
-            )
-
-            // 'Show more' or 'Continue thread' on reddit.com...
-          } else {
-            return (
-              <>
-              
-              </>
-            )
-          }
-        })}
-      </>
-    ) 
-  }
+  
 
   return (
     <div style={{position: 'relative'}}>
@@ -76,7 +37,13 @@ export const OpenPost = ({post}:  OpenPostProps) => {
         
         <div className='comment-wrapper'>
           {comments.length 
-            ? renderCommentsRecurse(comments)
+            ? comments.map((comment) => {
+              if (comment.kind === 't1') {
+                return <Comment comment={comment.data as CommentData} />
+              } else {
+                return <></>
+              }
+            })
             : <div>Loading comments...</div>}
         </div>
         
