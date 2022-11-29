624491f9f90f5182672410c0385734a69a67a578
Ryan1431 <ryan1431@gmail.com>
2022-11-29 12:58:51 -0500

    Change to single slider with single offset, increase selection radius over slide count circles

--- src/components/feed/post/Slide.tsx
+++ src/components/feed/post/Slide.tsx
@@ -17,11 +17,14 @@ export const Slide = ({slides}: SlideProps) => {
 
   return (
     <div className='slide-wrapper'>
-        {slides.map((s, i) => 
-          <div key={'inslide ' + s.u} style={{left: `${(i * 100) - (index * 100)}%`}} className='slide-image-wrapper'>
-            <img src={s.u} alt='slideshow'></img>  
-          </div>
-        )}
+        <div className='slider' style={{left: `-${index}00%`}}>
+          {slides.map((s, i) => 
+            <div className='slide-image-wrapper'>
+              <img key={'inslide ' + s.u} src={s.u} alt='slideshow'></img>  
+            </div>
+          )}
+        </div>
+
       <div className='slide-ui'>
         <div className={clsx('slide-move', 'left', {'max': !index})}
           onClick={() => setIndex(p => p - 1)}
@@ -36,7 +39,11 @@ export const Slide = ({slides}: SlideProps) => {
 
         <div className='slide-count-wrapper'>
           <div className='slide-count'>
-            {slides.map((x, i) => <div onClick={() => setIndex(i)} className={clsx('slide-count-circle', {'active': i === index})}></div>)}
+            {slides.map((x, i) => 
+              <div className='slide-circle-area' onClick={() => setIndex(i)} >
+                <div className={clsx('slide-count-circle', {'active': i === index})}></div>
+              </div>
+            )}
           </div>
         </div>
       </div>
